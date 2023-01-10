5e6f6bd9b7490b932b02883b11528fa65bb87e4c
Filigrani <filigranisb@gmail.com>
2023-01-11 02:12:56 +0400

    GithubManager added

---
+++ SkyCoopInstaller/GithubManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkyCoopInstaller
+{
+    internal class GithubManager
+    {
+        public const string MetadataURL = "https://api.github.com/repos/Filigrani/SkyCoop/releases";
+        public static string GitJson = "";
+
+        public static string RequestGithubData()
+        {
+            try
+            {
+                return Program.webClient.DownloadString(MetadataURL);
+            }
+            catch
+            {
+                return "";
+            }
+        }
+
+        public static void PrepareReleasesList() 
+        {
+            GitJson = RequestGithubData();
+            if (string.IsNullOrEmpty(GitJson))
+            {
+                return;
+            }
+
+        }
+    }
+}
--- SkyCoopInstaller/Program.cs
+++ SkyCoopInstaller/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -11,9 +12,13 @@ namespace SkyCoopInstaller
         /// <summary>
         /// Главная точка входа для приложения.
         /// </summary>
+        /// 
+        public static WebClient webClient;
+
         [STAThread]
         static void Main()
         {
+            webClient = new WebClient();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
